a507e66882eba79d6c02c499fb00fd771add8925
awiipp <sayidariipp@gmail.com>
2024-03-16 18:50:20 +0700

    Added: post migration, model  and db seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,27 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = \App\Models\User::factory()->create([
+            'firstname' => 'Kang',
+            'lastname' => 'Hyewon',
+            'username' => 'Hyemhyemu',
+            'password' => bcrypt('12345'),
+            'email' => 'khw@gmail.com',
+        ]);
+
+        $userId = $user->id;
+
+        \App\Models\Post::factory()->create([
+            'title' => 'Welcome to Portal Berita',
+            'news_content' => '<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam laborum quos expedita est facere itaque maiores, ea, unde quisquam dolorem aperiam eius fugiat magni vitae dignissimos. Laborum quod nisi est exercitationem necessitatibus officia atque eos unde, aliquid facilis veniam odio laboriosam tenetur beatae omnis soluta vel delectus nostrum consequuntur libero deserunt ex? Veniam tenetur doloribus cum, enim accusamus sapiente illo provident quibusdam porro reprehenderit earum nihil molestiae beatae ab suscipit ex labore nostrum nisi accusantium cupiditate, debitis distinctio nemo. Molestias eligendi nihil accusamus quod commodi eaque vitae ex. Ratione soluta laboriosam reiciendis atque voluptas voluptatibus qui pariatur! Quod, dicta accusamus!</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam laborum quos expedita est facere itaque maiores, ea, unde quisquam dolorem aperiam eius fugiat magni vitae dignissimos. Laborum quod nisi est exercitationem necessitatibus officia atque eos unde, aliquid facilis veniam odio laboriosam tenetur beatae omnis soluta vel delectus nostrum consequuntur libero deserunt ex? Veniam tenetur doloribus cum, enim accusamus sapiente illo provident quibusdam porro reprehenderit earum nihil molestiae beatae ab suscipit ex labore nostrum nisi accusantium cupiditate, debitis distinctio nemo. Molestias eligendi nihil accusamus quod commodi eaque vitae ex. Ratione soluta laboriosam reiciendis atque voluptas voluptatibus qui pariatur! Quod, dicta accusamus!</div>',
+            'author' => $userId
+        ]);
+
+        \App\Models\Post::factory()->create([
+            'title' => 'Announcement: Portal Berita',
+            'title' => 'Welcome to Portal Berita',
+            'news_content' => '<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam laborum quos expedita est facere itaque maiores, ea, unde quisquam dolorem aperiam eius fugiat magni vitae dignissimos. Laborum quod nisi est exercitationem necessitatibus officia atque eos unde, aliquid facilis veniam odio laboriosam tenetur beatae omnis soluta vel delectus nostrum consequuntur libero deserunt ex? Veniam tenetur doloribus cum, enim accusamus sapiente illo provident quibusdam porro reprehenderit earum nihil molestiae beatae ab suscipit ex labore nostrum nisi accusantium cupiditate, debitis distinctio nemo. Molestias eligendi nihil accusamus quod commodi eaque vitae ex. Ratione soluta laboriosam reiciendis atque voluptas voluptatibus qui pariatur! Quod, dicta accusamus!</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam laborum quos expedita est facere itaque maiores, ea, unde quisquam dolorem aperiam eius fugiat magni vitae dignissimos. Laborum quod nisi est exercitationem necessitatibus officia atque eos unde, aliquid facilis veniam odio laboriosam tenetur beatae omnis soluta vel delectus nostrum consequuntur libero deserunt ex? Veniam tenetur doloribus cum, enim accusamus sapiente illo provident quibusdam porro reprehenderit earum nihil molestiae beatae ab suscipit ex labore nostrum nisi accusantium cupiditate, debitis distinctio nemo. Molestias eligendi nihil accusamus quod commodi eaque vitae ex. Ratione soluta laboriosam reiciendis atque voluptas voluptatibus qui pariatur! Quod, dicta accusamus!</div>',
+            'author' => $userId
+        ]);
     }
 }
