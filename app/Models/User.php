7f37aff68c13fbbf0111d92e717b86514153f77d
awiipp <sayidariipp@gmail.com>
2024-03-17 12:02:55 +0700

    Added: relationship.

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Relationship
+     */
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
